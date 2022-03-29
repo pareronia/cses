b76616b1c73e258777c45ec13362605614649b4e
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-29 11:35:21 +0200

    1068 - Weird ALgorithm

---
+++ src/main/java/com/github/pareronia/cses/_1068/WeirdAlgorithm.java
@@ -0,0 +1,218 @@
+// package-start
+package com.github.pareronia.cses._1068;
+
+// package-end
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Weird Algorithm
+ * @see <a href="https://cses.fi/problemset/task/1068">https://cses.fi/problemset/task/1068</a>
+ */
+public class WeirdAlgorithm {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public WeirdAlgorithm(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+
+    public static final class CollatzSequence {
+
+        public static List<Long> list(final int n) {
+            long nn = n;
+            final List<Long> list = new ArrayList<>();
+            list.add(nn);
+            while (nn > 1) {
+                nn = next(nn);
+                list.add(nn);
+            }
+            return list;
+        }
+        
+        private static long next(final long n) {
+            if (n % 2 == 0) {
+                return n / 2;
+            } else {
+                return n * 3 + 1;
+            }
+        }
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final String ans = CollatzSequence.list(n).stream()
+                .map(String::valueOf)
+                .collect(joining(" "));
+        return new Result<>(i, List.of(ans));
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (this.sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results
+                    = Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(i, sc))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = WeirdAlgorithm.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new WeirdAlgorithm(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(WeirdAlgorithm.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("cses"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+}
