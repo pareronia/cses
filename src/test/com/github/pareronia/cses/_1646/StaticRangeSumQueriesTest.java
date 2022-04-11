fc1c8e7bb6ab64cc826b23012f87adb748d0f383
pareronia <49491686+pareronia@users.noreply.github.com>
2022-04-11 19:40:02 +0200

    1646 - Static Range Sum Queries

---
+++ src/test/com/github/pareronia/cses/_1646/StaticRangeSumQueriesTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.cses._1646;
+
+import com.github.pareronia.cses.TestBase;
+
+class StaticRangeSumQueriesTest extends TestBase<StaticRangeSumQueries> {
+
+    protected StaticRangeSumQueriesTest() {
+        super(StaticRangeSumQueries.class);
+    }
+}
